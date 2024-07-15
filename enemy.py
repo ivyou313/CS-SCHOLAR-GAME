fd9ef5f58a5a722adcba2313e6b94e96aa54f6ed
Natalia <160363625+Taliaswalia@users.noreply.github.com>
2024-07-15 15:05:01 -0400

    Update enemy.py

--- enemy.py
+++ enemy.py
@@ -5,26 +5,25 @@ import random
 
 screenWidth = 400
 screenHeight = 300
-width = 8.5
+width = 40
 speed = 7
-xPlaces = [100, 400]
 
 
-#Player
 class Enemy(pygame.sprite.Sprite):
     def __init__(self, xpos, ypos, finalPosx):
         super(Enemy, self,).__init__()
-        self.surf = pygame.Surface((width, width))
+        #random.choice
+        self.enemyShape = [pygame.image.load('3.png'),pygame.image.load('4.png'),pygame.image.load('5.png')]
+        self.surf = pygame.transform.scale(random.choice(self.enemyShape),(width, width))
         self.x = 7
         self.finalPosx = finalPosx
         #self.surf.fill((18, 107, 4))
         
-        self.surf.fill((95,87,120))
+       # self.surf.fill((95,87,120))
         self.rect = self.surf.get_rect (center=(xpos,ypos))
         
     
-    def update(self, time):
-          #random.choice(xPlaces)
+    def update(self):
           dx, dy = self.finalPosx - self.rect.x, 400 - self.rect.y
           dist = math.hypot(dx, dy)
           if round(dist) > 1:
@@ -34,8 +33,9 @@ class Enemy(pygame.sprite.Sprite):
             self.rect.x += dx * 2
             self.rect.y += dy * 2
             pygame.transform.scale(self.surf, (self.x, self.x))
-           # self.surf = pygame.Surface(width*(time%10), width*(time%10))
-            self.surf.fill((18, 107, 4))
+            # size = round((width*(self.x))*2)
+            # self.surf = pygame.transform.scale(random.choice(self.enemyShape),(size, size))
+            #self.surf.fill((18, 107, 4))
 
           else:
             self.kill()
